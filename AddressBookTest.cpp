775fd33a0ccf96d31509521f6969ab0125554f44
maievskyi <maevua2@gmail.com>
2025-09-10 13:14:22 +0300

    setlocale(LC_ALL, "Rus")

--- AddressBookTest.cpp
+++ AddressBookTest.cpp
@@ -13,7 +13,7 @@ int main()
     SetConsoleCP(1251);     // my +  #include <windows.h>   //my
     SetConsoleOutputCP(1251); // my +  #include <windows.h>   //my
 	//system("chcp 1251");
-	//setlocale(LC_ALL, "Rus"); /* Обязательно должно быть первым........ */
+	setlocale(LC_ALL, "Rus"); /* Обязательно должно быть первым........ */
 
 	std::cout << "*** Русский текст в cout ***\n";
 
