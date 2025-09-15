4e2f1a8264ae4f4ce3b2a5b68fe056be5979dfcf
maievskiy <maevua@gmail.com>
2025-09-15 10:43:34 +0300

    UTF-8 UTF-8 vith boom

--- AddressBookTest.cpp
+++ AddressBookTest.cpp
@@ -1,66 +1,66 @@
-// TinyPIM (c) 1999 Pablo Halpern. File AddressBookTest.cpp
+п»ї// TinyPIM (c) 1999 Pablo Halpern. File AddressBookTest.cpp
 
 //#define _CRT_SECURE_NO_WARNINGS  //my orfogr mistakes ???  my
 #include <windows.h>//my
 #include <iostream>
 #include "AddressBook.h"
-#include <locale.h> /* Для русского языка ...........................*/
-// проверка на кирилицу UTF-8
+#include <locale.h> /* Р”Р»СЏ СЂСѓСЃСЃРєРѕРіРѕ СЏР·С‹РєР° ...........................*/
+// РїСЂРѕРІРµСЂРєР° РЅР° РєРёСЂРёР»РёС†Сѓ UTF-8 UTF-8
 
 
 int main()
 {
-    SetConsoleCP(1251);     // my +  #include <windows.h>   //my
-    SetConsoleOutputCP(1251); // my +  #include <windows.h>   //my
+    //SetConsoleCP(1251);     // my +  #include <windows.h>   //my
+   // SetConsoleOutputCP(1251); // my +  #include <windows.h>   //my
 	//system("chcp 1251");
-	setlocale(LC_ALL, "Rus"); /* Обязательно должно быть первым........ */
+	setlocale(LC_ALL, "Rus"); /* РћР±СЏР·Р°С‚РµР»СЊРЅРѕ РґРѕР»Р¶РЅРѕ Р±С‹С‚СЊ РїРµСЂРІС‹Рј........ */
 
-	std::cout << "*** Русский текст в cout ***\n";
+	std::cout << "*** Р СѓСЃСЃРєРёР№ С‚РµРєСЃС‚ РІ cout ***\n";
 
-  AddressBook book;     // конструктором класса AddressBook создаётся объект book
+  AddressBook book;     // РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂРѕРј РєР»Р°СЃСЃР° AddressBook СЃРѕР·РґР°С‘С‚СЃСЏ РѕР±СЉРµРєС‚ book
  
-  Address a;  //созд-е объекта а (=записи=адреса)
-  a.lastname("Smith"); // заполнение полей с помощью соответствующих 
-  a.firstname("Joan");      // перегруженных методов класса Address
+  Address a;  //СЃРѕР·Рґ-Рµ РѕР±СЉРµРєС‚Р° Р° (=Р·Р°РїРёСЃРё=Р°РґСЂРµСЃР°)
+  a.lastname("Smith"); // Р·Р°РїРѕР»РЅРµРЅРёРµ РїРѕР»РµР№ СЃ РїРѕРјРѕС‰СЊСЋ СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС‰РёС… 
+  a.firstname("Joan");      // РїРµСЂРµРіСЂСѓР¶РµРЅРЅС‹С… РјРµС‚РѕРґРѕРІ РєР»Р°СЃСЃР° Address
   a.phone("(617) 555-9876");
   a.address("The Very Big Corporation\nSomewhere, MA 01000");
 
-  Address b;  //созд-е объекта b (=записи=адреса)
+  Address b;  //СЃРѕР·Рґ-Рµ РѕР±СЉРµРєС‚Р° b (=Р·Р°РїРёСЃРё=Р°РґСЂРµСЃР°)
   b.lastname("Adams");
   b.firstname("Abigale");
   b.phone("(212) 555-3734");
   b.address("743 Broadway\nNew York, NY");
 
-  Address c;  //созд-е объекта c (=записи=адреса)
+  Address c;  //СЃРѕР·Рґ-Рµ РѕР±СЉРµРєС‚Р° c (=Р·Р°РїРёСЃРё=Р°РґСЂРµСЃР°)
   c.lastname("Neighborhood Video");
   c.phone("555-FILM");
 
-  // в переменных сохр id номера записей AddressBook
+  // РІ РїРµСЂРµРјРµРЅРЅС‹С… СЃРѕС…СЂ id РЅРѕРјРµСЂР° Р·Р°РїРёСЃРµР№ AddressBook
   int a_id = book.insertAddress(a);
   int b_id = book.insertAddress(b);
   int c_id = book.insertAddress(c);
   std::cout << "*** Three Address Entries ***\n";
   book.print();
 
-  //созд-е объекта d с дублированием 1го имени и 2го имени с объектом b
-  //     Адрес d имеет те же имена в полях, что и b
+  //СЃРѕР·Рґ-Рµ РѕР±СЉРµРєС‚Р° d СЃ РґСѓР±Р»РёСЂРѕРІР°РЅРёРµРј 1РіРѕ РёРјРµРЅРё Рё 2РіРѕ РёРјРµРЅРё СЃ РѕР±СЉРµРєС‚РѕРј b
+  //     РђРґСЂРµСЃ d РёРјРµРµС‚ С‚Рµ Р¶Рµ РёРјРµРЅР° РІ РїРѕР»СЏС…, С‡С‚Рѕ Рё b
   Address d;    
   d.lastname("Adams");
   d.firstname("Abigale");
   d.phone("(508) 555-4466");
   d.address("1 Small St.\nMarlboro, MA 02100");
 
-  // Insert address with duplicate name вставка адреса с дубл именем
-  int d_id = book.insertAddress(d);  // сохр в AddressBook
+  // Insert address with duplicate name РІСЃС‚Р°РІРєР° Р°РґСЂРµСЃР° СЃ РґСѓР±Р» РёРјРµРЅРµРј
+  int d_id = book.insertAddress(d);  // СЃРѕС…СЂ РІ AddressBook
   std::cout << "*** After adding a duplicate Abigale Adams ***\n";
-  book.print();  // появляется запись в книгу с новым id
+  book.print();  // РїРѕСЏРІР»СЏРµС‚СЃСЏ Р·Р°РїРёСЃСЊ РІ РєРЅРёРіСѓ СЃ РЅРѕРІС‹Рј id
 
-  // Erase an address удаление
+  // Erase an address СѓРґР°Р»РµРЅРёРµ
   book.eraseAddress(a_id);
   std::cout << "*** After erasing Joan Smith ***\n";
   book.print();
 
-  // Replace an address исправление
+  // Replace an address РёСЃРїСЂР°РІР»РµРЅРёРµ
   c.address("22 Main St.\nMy town, MA 02200");
   book.replaceAddress(c, c_id);
   std::cout << "*** After replacing Neighborhood Video ***\n";
